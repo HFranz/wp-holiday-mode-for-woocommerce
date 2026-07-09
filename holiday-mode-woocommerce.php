7bbd18b4fa033543abd448db5bb47d29c5410074
Heinrich Franz <heinrich.franz@sevmatic.com>
2026-07-09 21:49:01 +0200

    Update required versions in holiday-mode-woocommerce.php

--- holiday-mode-woocommerce.php
+++ holiday-mode-woocommerce.php
@@ -23,8 +23,8 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       holiday-mode-woocommerce
  * Domain Path: 	  /languages
- * Requires at least: 5.4
- * Requires PHP:      7.3
+ * Requires at least: 6.7
+ * Requires PHP:      8.4
  */
 
 /*
